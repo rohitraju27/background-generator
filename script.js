25c9575e180b9a89b659e53042ea9ea94b9a18e6
unknown <rohu270700@gmail.com>
2020-08-04 19:11:11 +0530

    added a line below the button

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ var body = document.getElementById("gradient");
 var btn = document.getElementById("btn");
 var a = document.getElementById("a");
 var b = document.getElementById("b");
+var demo = document.getElementById("demo");
+var text = "Now change the background colors to generate random  numbers!!!"
 
 function setGradient(){
 	body.style.background = "linear-gradient(to right, " +
@@ -26,6 +28,7 @@ color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
 
 btn.addEventListener("click", function(){
+	demo.textContent = text;
 	color1.addEventListener("input", addRandomA);
 	color2.addEventListener("input", addRandomB);
 });
